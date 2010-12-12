4aeef5e0dcb24d7d98d9078eabc6ac2e34494490
sunjw8888 <sunjw8888@54c89845-c17d-54a0-e9b2-79b530de6786>
2010-12-12 11:26:09 +0000

    更新版权说明

--- trunk/jsmin.cpp
+++ trunk/jsmin.cpp
@@ -1,7 +1,9 @@
-/* jsmin.c
+/* jsmin.cpp
    2008-08-03
+   2010-09
 
 Copyright (c) 2002 Douglas Crockford  (www.crockford.com)
+Copyright (c) 2010 Sun Junwen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
