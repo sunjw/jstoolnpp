41ac281f2bc56ff6b8dba9655124bfa06d5d6941
sunjw8888 <sunjw8888@54c89845-c17d-54a0-e9b2-79b530de6786>
2011-04-17 01:51:03 +0000

    更新 strconvert

--- strconvert.cpp
+++ strconvert.cpp
@@ -3,6 +3,22 @@
 #include "strconvert.h"
 
 /*
+ * 将 ascii 编码的 wstring 转换为 utf8 编码的 string
+ */
+std::string wstrtostrutf8(const std::wstring &wstr)
+{
+    // Convert a Unicode string to an ASCII string
+	size_t strLen = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), (int)wstr.length(), NULL, 0, NULL, NULL);
+    std::string strTo;
+    char *szTo = new char[strLen + 1];
+    szTo[strLen] = '\0';
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, szTo, (int)strLen, NULL, NULL);
+    strTo = szTo;
+    delete[] szTo;
+    return strTo;
+}
+
+/*
  * 将 utf8 编码的 string 转换为 wstring
  * 如果需要把 utf8 编码的 string 转换为本地编码的 string
  * 在调用一次 wstrtostr
--- strconvert.h
+++ strconvert.h
@@ -2,6 +2,7 @@
 #define _STRCONVERT_H_
 #include <string>
 
+std::string wstrtostrutf8(const std::wstring &wstr);
 std::wstring strtowstrutf8(const std::string &str);
 std::string wstrtostr(const std::wstring &wstr);
 std::wstring strtowstr(const std::string &str);
