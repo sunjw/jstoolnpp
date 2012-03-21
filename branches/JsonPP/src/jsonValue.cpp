9f09d80ea399b0d5c4329b482892f34efd2abcb6
sunjw8888 <sunjw8888@54c89845-c17d-54a0-e9b2-79b530de6786>
2012-03-21 05:29:43 +0000

    增加版权和授权声明

--- branches/JsonPP/src/jsonValue.cpp
+++ branches/JsonPP/src/jsonValue.cpp
@@ -1,7 +1,23 @@
-/*
- * JsonValue class header file
- * Author: Sun Junwen
- */
+/* JsonValue.cpp
+   2012-3-11
+   Version: 0.9
+
+Copyright (c) 2012 SUN Junwen
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 #include <cstdlib>
 #include <string>
 #include <map>
