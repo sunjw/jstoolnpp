a4b35d542789e7d2024891290f0233e6a6d74342
sunjw8888 <sunjw8888@54c89845-c17d-54a0-e9b2-79b530de6786>
2012-03-31 07:56:41 +0000

    调整 JsonViewer 修改版本号

--- trunk/JSONDialog.cpp
+++ trunk/JSONDialog.cpp
@@ -210,6 +210,11 @@ void JSONDialog::insertJsonValue(const string& key, const JsonValue& jsonValue, 
 	if(valType == JsonValue::MAP_VALUE ||
 		valType == JsonValue::ARRAY_VALUE)
 	{
+		if(valType == JsonValue::MAP_VALUE)
+			tstr.append(TEXT(" : [Object]"));
+		if(valType == JsonValue::ARRAY_VALUE)
+			tstr.append(TEXT(" : [Array]"));
+
 		HTREEITEM newNode = insertTree(tstr.c_str(), node);
 		insertJsonValue(jsonValue, newNode);
 	}
