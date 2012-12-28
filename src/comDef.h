3f75488e7e853eebb54bc2f798302f23b4a0e351
sunjw8888@gmail.com <sunjw8888@gmail.com@54c89845-c17d-54a0-e9b2-79b530de6786>
2012-12-28 13:37:39 +0000

    发布 1.12 将网站转移到 sunjw.us

--- src/comDef.h
+++ src/comDef.h
@@ -2,8 +2,8 @@
 #define _COMMON_DEF_H_
 
 #define PLUGIN_NAME "&JSMin"
-#define PROJECT_SITE "http://jsminnpp.sourceforge.net"
-#define CHECK_UPDATE "http://jsminnpp.sourceforge.net/download.php"
+#define PROJECT_SITE "http://www.sunjw.us/jsminnpp/"
+#define CHECK_UPDATE "http://www.sunjw.us/jsminnpp/download.php"
 #define DONATION "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sunjw8888%40yahoo%2ecom%2ecn&item_name=JSMinNpp%20Project&no_shipping=1&cn=Optional%20comments&tax=0&currency_code=USD&lc=US&amount=1.8&bn=PP%2dDonationsBF&charset=UTF%2d8"
 
 #endif
