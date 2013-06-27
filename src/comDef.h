6f41fc952636cfa81caace34b51b1f7e89702b2d
sunjw8888@gmail.com <sunjw8888@gmail.com@54c89845-c17d-54a0-e9b2-79b530de6786>
2013-06-27 15:14:43 +0000

    发布 1.15 改名为 JSTool

--- src/comDef.h
+++ src/comDef.h
@@ -1,7 +1,7 @@
 #ifndef _COMMON_DEF_H_
 #define _COMMON_DEF_H_
 
-#define PLUGIN_NAME "&JSMin"
+#define PLUGIN_NAME "&JSTool"
 #define PROJECT_SITE "http://www.sunjw.us/jsminnpp/"
 #define CHECK_UPDATE "http://www.sunjw.us/jsminnpp/download.php"
 #define DONATION "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sunjw8888%40yahoo%2ecom%2ecn&item_name=JSMinNpp%20Project&no_shipping=1&cn=Optional%20comments&tax=0&currency_code=USD&lc=US&amount=1.8&bn=PP%2dDonationsBF&charset=UTF%2d8"
