a983249b97c8e935a9da4de5a95b042e10b4a3e3
Sun Junwen <sunjw8888@gmail.com>
2015-05-08 09:57:07 +0800

    Fix fstream on Windows/*nix compile.

--- trunk/src/IniFileProcessor.cpp
+++ trunk/src/IniFileProcessor.cpp
@@ -3,17 +3,24 @@
  * Author: Sun Junwen
  * Version: 1.0.1
  */
+#include "IniFileProcessor.h"
+
 #include <cstdlib>
 #include <string>
 #include <fstream>
-
-#include "IniFileProcessor.h"
+#include "strhelper.h"
 
 using namespace std;
+using namespace sunjwbase;
 
 void IniFileProcessor::Save()
 {
+#if defined (WIN32)
 	ofstream out(m_tstrFileName.c_str());
+#else
+	string strFileName = tstrtostr(m_tstrFileName);
+	ofstream out(strFileName.c_str());
+#endif
 
 	if(out)
 	{
@@ -25,6 +32,11 @@ void IniFileProcessor::Save()
 	
 IniProcessor::IniMap IniFileProcessor::GetInfo(bool bProcSection, bool bRefresh)
 {
+#if defined (WIN32)
 	ifstream m_in(m_tstrFileName.c_str());
+#else
+	string strFileName = tstrtostr(m_tstrFileName);
+	ifstream m_in(strFileName.c_str());
+#endif
 	return IniProcessor::GetInfo(m_in, bProcSection, bRefresh);
 }
