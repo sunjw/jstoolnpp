5e6b9b98bf8c970dd2f9f17169f46dd2f1e52d72
Sun Junwen <sunjw8888@gmail.com>
2015-09-15 08:29:54 +0800

    Implement FormatJavaScript function.

--- trunk/src/jsformatter.cpp
+++ trunk/src/jsformatter.cpp
@@ -7,5 +7,12 @@ DLLAPI void FormatJavaScript(void *ioContext,
 							 WriteCharFunc writeCharFunc,
 							 const FormatterOption *option)
 {
-	
+	JSFormatGenericIO jsf(ioContext, readCharFunc, writeCharFunc, 
+							FormatterOption(option->chIndent,
+											option->nChPerInd,
+											option->eCRRead,
+											option->eCRPut,
+											option->eBracNL,
+											option->eEmpytIndent));
+	jsf.Go();
 }
