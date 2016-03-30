e299bdc8da3ec95af9752718b953d6118d295dbc
Sun Junwen <sunjw8888@gmail.com>
2016-03-30 11:20:23 +0800

    Add some test cases.

--- sub-proj/JsonPP/tests/in.js
+++ sub-proj/JsonPP/tests/in.js
@@ -1,3 +1,14 @@
 {
-	"kjsldfjds" : /(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?/
+	"zh_CN" : "测试",
+	"jp" : "テスト",
+	"hu_HU" : "Sűrű szövésű női pulóver",
+	"Sűrű szövésű női pulóver" : {
+		"zh_CN" : "测试",
+		"jp" : "テスト",
+		"hu_HU" : "Sűrű szövésű női pulóver"
+	},
+	"测试" : {
+		xxx : 1,
+		"Sűrű szövésű női pulóver" : "テスト"
+	}
 }
