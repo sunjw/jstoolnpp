6d492c860dc059a96b66c582b1bd36d95503ab3d
Sun Junwen <sunjw8888@gmail.com>
2016-06-16 13:02:21 +0800

    Update JsonPP test cases format.

--- sub-proj/JsonPP/tests/in2.js
+++ sub-proj/JsonPP/tests/in2.js
@@ -1,38 +1,38 @@
 [{
-		"glossary" : {
-			"title" : "example glossary",
-			"GlossDiv" : {
-				"title" : "S",
-				"GlossList" : {
-					"GlossEntry" : {
-						"ID" : "SGML",
-						"SortAs" : "SGML",
-						"GlossTerm" : "Standard Generalized Markup Language",
-						"Acronym" : "SGML",
-						"Abbrev" : "ISO 8879:1986",
-						"GlossDef" : {
-							"para" : "A meta-markup language, used to create markup languages such as DocBook.",
-							"GlossSeeAlso" : ["GML", "XML"]
+		"glossary": {
+			"title": "example glossary",
+			"GlossDiv": {
+				"title": "S",
+				"GlossList": {
+					"GlossEntry": {
+						"ID": "SGML",
+						"SortAs": "SGML",
+						"GlossTerm": "Standard Generalized Markup Language",
+						"Acronym": "SGML",
+						"Abbrev": "ISO 8879:1986",
+						"GlossDef": {
+							"para": "A meta-markup language, used to create markup languages such as DocBook.",
+							"GlossSeeAlso": ["GML", "XML"]
 						},
-						"GlossSee" : "markup"
+						"GlossSee": "markup"
 					}
 				}
 			}
 		}
 	}, {
-		"menu" : {
-			"id" : "file",
-			"value" : "File",
-			"popup" : {
-				"menuitem" : [{
-						"value" : "New",
-						"onclick" : "CreateNewDoc()"
+		"menu": {
+			"id": "file",
+			"value": "File",
+			"popup": {
+				"menuitem": [{
+						"value": "New",
+						"onclick": "CreateNewDoc()"
 					}, {
-						"value" : "Open",
-						"onclick" : "OpenDoc()"
+						"value": "Open",
+						"onclick": "OpenDoc()"
 					}, {
-						"value" : "Close",
-						"onclick" : "CloseDoc()"
+						"value": "Close",
+						"onclick": "CloseDoc()"
 					}
 				]
 			}
