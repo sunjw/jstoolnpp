a08a5b046617033dc840d4b11f24bc4eb946904e
Wayne Sun <wayne_sun@trendmicro.com.cn>
2017-11-27 17:14:13 +0800

    Update test python script.

--- sub-proj/JSFormatterTest/tests/test.py
+++ sub-proj/JSFormatterTest/tests/test.py
@@ -11,10 +11,10 @@ import time
 import collections
 from subprocess import call
 
-JSFORMATTER_PATH_WIN = "..\\..\\..\\trunk\\debug\\JSFormatterTest.exe"
-JSFORMATTER_REL_PATH_WIN = "..\\..\\..\\trunk\\release\\JSFormatterTest.exe"
-JSFORMATTER_PATH_WIN_64 = "..\\..\\..\\trunk\\x64\\debug\\JSFormatterTest.exe"
-JSFORMATTER_REL_PATH_WIN_64 = "..\\..\\..\\trunk\\x64\\release\\JSFormatterTest.exe"
+JSFORMATTER_PATH_WIN = "../../../trunk/debug/JSFormatterTest.exe"
+JSFORMATTER_REL_PATH_WIN = "../../../trunk/release/JSFormatterTest.exe"
+JSFORMATTER_PATH_WIN_64 = "../../../trunk/x64/debug/JSFormatterTest.exe"
+JSFORMATTER_REL_PATH_WIN_64 = "../../../trunk/x64/release/JSFormatterTest.exe"
 JSFORMATTER_LIB_PATH_MAC = "../../../trunk/DerivedData/JSTool/Build/Products/Debug"
 JSFORMATTER_LIB_REL_PATH_MAC = "../../../trunk/DerivedData/JSTool/Build/Products/Release"
 JSFORMATTER_PATH_MAC = "../../../trunk/DerivedData/JSTool/Build/Products/Debug/JSFormatterTest"
@@ -76,6 +76,7 @@ def run_case(test_case, release):
 
 	# os.system(JSFORMATTER_PATH_SEL + " " + test_case.source + " out.js")
 	call([JSFORMATTER_PATH_SEL, test_case.source, "out.js"])
+
 	out_md5 = hashlib.md5(open("out.js").read()).hexdigest()
 	result_md5 = hashlib.md5(open(test_case.result).read()).hexdigest()
 	if out_md5 == result_md5:
@@ -84,30 +85,34 @@ def run_case(test_case, release):
 	print result
 	return result
 
+def dump_version():
+	global JSFORMATTER_PATH_SEL
+
+	call([JSFORMATTER_PATH_SEL, "--version"])
+
 def main():
 	global JSFORMATTER_PATH_SEL
 
+	if not is_windows_sys() and not is_osx_sys():
+		print "Unknown operating system."
+		return
+
 	files = list_file()
 	test_cases = make_test_case(files)
+
 	x64 = False
 	release = False
 
-	if is_windows_sys():
-		if len(sys.argv) == 2:
-			if sys.argv[1] == "release":
-				release = True
-			elif sys.argv[1] == "64":
-				x64 = True
-
-		if len(sys.argv) == 3 and ((sys.argv[1] == "release" and sys.argv[2] == "64") or (sys.argv[2] == "release" and sys.argv[1] == "64")):
+	for argv in sys.argv:
+		argv = argv.lower()
+		if argv == "release":
 			release = True
+		if argv == "64" or argv == "x64":
 			x64 = True
-	else:
-		if len(sys.argv) == 2 and sys.argv[1] == "release":
-			release = True
 
 	JSFORMATTER_LIB_PATH_SEL = ""
 	if is_osx_sys():
+		x64 = False # no x64 on macOS
 		JSFORMATTER_LIB_PATH_SEL = JSFORMATTER_LIB_PATH_MAC
 		if release:
 			JSFORMATTER_LIB_PATH_SEL = JSFORMATTER_LIB_REL_PATH_MAC
@@ -122,7 +127,7 @@ def main():
 			JSFORMATTER_PATH_SEL = JSFORMATTER_PATH_WIN_64
 		elif release:
 			JSFORMATTER_PATH_SEL = JSFORMATTER_REL_PATH_WIN
-	else:
+	elif is_osx_sys():
 		JSFORMATTER_PATH_SEL = JSFORMATTER_PATH_MAC
 		if release:
 			JSFORMATTER_PATH_SEL = JSFORMATTER_REL_PATH_MAC
@@ -153,11 +158,15 @@ def main():
 	if allpass:
 		print "%d cases ALL PASS, took %.2fs." % (len(test_cases), duration_time)
 
+	print "Test args: x64=%r, release=%r" % (x64, release)
 	print ""
+
 	if is_osx_sys():
-		print "DYLD_LIBRARY_PATH=" + os.environ["DYLD_LIBRARY_PATH"]
-	print "Using " + JSFORMATTER_PATH_SEL
-	call([JSFORMATTER_PATH_SEL, "--version"])
+		print "DYLD_LIBRARY_PATH=%s" % (os.environ["DYLD_LIBRARY_PATH"])
+	else:
+		print "Using %s" % (JSFORMATTER_PATH_SEL)
+
+	dump_version()
 
 if __name__ == '__main__':
 	main()
