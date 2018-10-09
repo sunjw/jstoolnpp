30bb6c6f0ff69cc92a7865a9f12f9820eb08fc7a
Sun Junwen <sunjw8888@gmail.com>
2018-10-09 18:00:11 +0800

    JSFormatterJS: Update version to 1.22.4.

--- sub-proj/JSFormatterJS/realjsformatter.js
+++ sub-proj/JSFormatterJS/realjsformatter.js
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 const JSParser = require('./jsparser.js');
 
-const VERSION = "1.22.3.0";
+const VERSION = "1.22.4.0";
 
 function Trim(str) {
     return str.trim();
