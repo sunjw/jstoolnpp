1fc82bc4c8e6665c61f48f49ce707b0833327b2b
Sun Junwen <sunjw8888@gmail.com>
2018-10-10 17:14:19 +0800

    JSFormatterJS: Change extension register command to "Format JS".

--- sub-proj/JSFormatterJS/extension.js
+++ sub-proj/JSFormatterJS/extension.js
@@ -8,12 +8,12 @@ function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "testhello" is now active!');
+    console.log('Congratulations, your extension "jstool" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
+    let disposable = vscode.commands.registerCommand('extension.formatJS', function () {
         // The code you place here will be executed every time your command is executed
 
         let editor = vscode.window.activeTextEditor;
