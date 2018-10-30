b0209d60281a08a3d40780ff51f8d093cc552fb1
Sun Junwen <sunjw8888@gmail.com>
2018-10-30 10:40:37 +0800

    Update version to 1.1810.3.

--- sub-proj/JSFormatterJS/realjsformatter.js
+++ sub-proj/JSFormatterJS/realjsformatter.js
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 const JSParser = require('./jsparser.js');
 
-const VERSION = "1.1810.2.0";
+const VERSION = "1.1810.3.0";
 
 function Trim(str) {
     return str.trim();
