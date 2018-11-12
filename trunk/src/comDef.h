396241c610aa4eeb679b3a43cb572906cd8d7c23
Sun Junwen <sunjw8888@gmail.com>
2018-11-12 13:17:54 +0800

    Change project url in npp menu.

--- trunk/src/comDef.h
+++ trunk/src/comDef.h
@@ -16,8 +16,8 @@
 #define STRING_NEW_VERSION "&New version!"
 #define STRING_ABOUT "&About"
 
-#define PROJECT_SITE "http://www.sunjw.us/jstoolnpp/"
-#define CHECK_UPDATE "http://www.sunjw.us/jstoolnpp/download.php"
+#define PROJECT_SITE "http://www.sunjw.us/jstool/"
+#define CHECK_UPDATE "http://www.sunjw.us/jstool/"
 #define UPDATE_FILE_URL "https://raw.githubusercontent.com/sunjw/jstoolnpp/master/trunk/ReleasedFiles/ReleaseVersion.json"
 #define DONATION "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=sunjw8888%40yahoo%2ecom%2ecn&item_name=JSMinNpp%20Project&no_shipping=1&cn=Optional%20comments&tax=0&currency_code=USD&lc=US&amount=1.8&bn=PP%2dDonationsBF&charset=UTF%2d8"
 #define GITHUB_PAGE "https://github.com/sunjw/jstoolnpp"
