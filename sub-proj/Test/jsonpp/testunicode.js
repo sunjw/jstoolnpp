34ca94cecba18471e67561fcbbf47e1eba97cd38
Sun Junwen <sunjw8888@gmail.com>
2019-01-15 10:16:57 +0800

    Change test case.

--- sub-proj/Test/jsonpp/testunicode.js
+++ sub-proj/Test/jsonpp/testunicode.js
@@ -3,9 +3,9 @@
 	"jp": "テスト",
 	"hu_HU": "Sűrű szövésű női pulóver",
 	"Sűrű szövésű női pulóver": {
-		"zh_CN": "测试",
-		"jp": "テスト",
-		"hu_HU": "Sűrű szövésű női pulóver"
+		"zh_CN": "测\\试 최'시'원 원\"장\"",
+		"jp": 'テ\'ス\'トテ\\トト',
+		"hu_HU": "Sűrű szö\"v'é's\"ű női pul\\/óver"
 	},
 	"测试": {
 		xxx: 1,
--- sub-proj/Test/jsonpp/testunicode.sort.test.js
+++ sub-proj/Test/jsonpp/testunicode.sort.test.js
@@ -1,8 +1,8 @@
 {
 	"Sűrű szövésű női pulóver": {
-		"hu_HU": "Sűrű szövésű női pulóver",
-		"jp": "テスト",
-		"zh_CN": "测试"
+		"hu_HU": "Sűrű szö\"v'é's\"ű női pul\\/óver",
+		"jp": "テ'ス'トテ\\トト",
+		"zh_CN": "测\\试 최'시'원 원\"장\""
 	},
 	"hu_HU": "Sűrű szövésű női pulóver",
 	"jp": "テスト",
