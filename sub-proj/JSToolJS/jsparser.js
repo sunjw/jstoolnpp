575f537bf51dc2b179c2ffffa09a693a9c8cdeac
Sun Junwen <sunjw8888@gmail.com>
2019-05-12 15:13:21 +0800

    Update version to 1.1905.1.

--- sub-proj/JSToolJS/jsparser.js
+++ sub-proj/JSToolJS/jsparser.js
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 'use strict';
 
-const VERSION = "1.1905.0.0";
+const VERSION = "1.1905.1.0";
 
 function StringReplaceAll(string, target, replace) {
     return string.replace(new RegExp(target, 'g'), replace);
