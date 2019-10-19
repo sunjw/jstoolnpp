356a8d25bf8cacb531683a36ba77a983d5ee2137
Sun Junwen <sunjw8888@gmail.com>
2019-10-19 23:54:26 +0800

    site: Update favicon url with https enabled.

--- sub-proj/JSToolSite/inc/head.part.php
+++ sub-proj/JSToolSite/inc/head.part.php
@@ -1,5 +1,5 @@
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
 <meta name="keywords" content="JSTool, JSToolNpp, JSMinNpp, Notepad++, npp, Visual Studio Code, vsc, extension, plugin, JavaScript, js, jsmin, js minimize, js format, JSON, json viewer, json sort, SourceForge, GitHub, open source, Sun Junwen, sunjw8888" />
-<link rel="shortcut icon" href="http://www.sunjw.us/jstool/favicon.ico" type="image/vnd.microsoft.icon">
-<link rel="icon" href="http://www.sunjw.us/jstool/favicon.ico" type="image/vnd.microsoft.icon">
+<link rel="shortcut icon" href="//www.sunjw.us/jstool/favicon.ico" type="image/vnd.microsoft.icon">
+<link rel="icon" href="//www.sunjw.us/jstool/favicon.ico" type="image/vnd.microsoft.icon">
 <link href="../css/style.css" rel="stylesheet" type="text/css" />
