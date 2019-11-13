16ddcaa8667aab49956e7750539d37b119e2b72c
Sun Junwen <sunjw8888@gmail.com>
2019-11-14 00:16:22 +0800

    vsc: Fix regular expression with "=>".

--- sub-proj/JSToolJS/jsparser.js
+++ sub-proj/JSToolJS/jsparser.js
@@ -107,7 +107,7 @@ class JSParser {
         this.m_debug = false;
         this.m_lineCount = 1;
         this.m_tokenCount = 0;
-        this.m_strBeforeReg = "(,=:[!&|?+{};\n";
+        this.m_strBeforeReg = "(,=:[!&|?+{};>\n";
         this.m_bRegular = false;
         this.m_iRegBracket = 0;
         this.m_bPosNeg = false;
