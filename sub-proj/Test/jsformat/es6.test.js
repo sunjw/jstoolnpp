0ccdf43987e05e85c20949422be8dd9584da8639
Sun Junwen <sunjw8888@gmail.com>
2021-07-12 11:13:21 +0800

    test: Update test case for new ES operator.

--- sub-proj/Test/jsformat/es6.test.js
+++ sub-proj/Test/jsformat/es6.test.js
@@ -522,6 +522,52 @@ class Clz {
 let Obj = {
 	 * foo() {
 		y = zxx;
+
+		const nullValue = null;
+		const emptyText = ""; // falsy
+		const someNumber = 42;
+
+		const valA = nullValue ?? "default for A";
+		const valB = emptyText ?? "default for B";
+		const valC = someNumber ?? 0;
+
+		console.log(valA); // "default for A"
+		console.log(valB); // "" (as the empty string is not null or undefined)
+		console.log(valC); // 42
+
+		let myText = ''; // An empty string (which is also a falsy value)
+
+		let notFalsyText = myText || 'Hello world';
+		console.log(notFalsyText); // Hello world
+
+		let preservingFalsy = myText ?? 'Hi neighborhood';
+		console.log(preservingFalsy); // '' (as myText is neither undefined nor null)
+
+		function A() {
+			console.log('A was called');
+			return undefined;
+		}
+		function B() {
+			console.log('B was called');
+			return false;
+		}
+		function C() {
+			console.log('C was called');
+			let foo = (null || undefined) ?? "foo"; // returns "foo"
+
+			console.log(foo.someFooProp?.toUpperCase() ?? "not available"); // "HI"
+			console.log(foo.someBarProp?.toUpperCase() ?? "not available"); // "not available"
+			return "foo";
+		}
+
+		console.log(A() ?? C());
+		// logs "A was called" then "C was called" and then "foo"
+		// as A() returned undefined so both expressions are evaluated
+
+		console.log(B() ?? C());
+		// logs "B was called" then "false"
+		// as B() returned false (and not null or undefined), the right
+		// hand side expression was not evaluated
 	}
 };
 
