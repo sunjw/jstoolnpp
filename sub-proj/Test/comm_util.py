18da32e47b05bb4c9eb8970e242440d4ec528fc0
Sun Junwen <sunjw8888@gmail.com>
2021-08-25 13:04:55 +0800

    test: Update comm_util.

--- sub-proj/Test/comm_util.py
+++ sub-proj/Test/comm_util.py
@@ -81,6 +81,17 @@ def call_command(args_list, is_shell=False):
     subprocess.call(args_list, shell=is_shell)
 
 
+def call_command_with_output(args_list, is_shell=False):
+    process = subprocess.Popen(args_list, shell=is_shell,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+    # wait for the process to terminate
+    stdout, stderr = process.communicate()
+    returncode = process.returncode
+    return (returncode, stdout.decode('utf-8'), stderr.decode('utf-8'))
+
+
 def md5_str(input_str, encode='utf-8'):
     md5hash = hashlib.md5()
     md5hash.update(input_str.encode(encode))
@@ -90,3 +101,32 @@ def md5_str(input_str, encode='utf-8'):
 def md5_file(file_path):
     filemd5 = hashlib.md5(open(file_path, 'rb').read()).hexdigest()
     return filemd5
+
+
+def query_yes_no(question, default='yes'):
+    # Ask a yes/no question via raw_input() and return their answer.
+    # "question" is a string that is presented to the user.
+    # "default" is the presumed answer if the user just hits <Enter>.
+    #         It must be "yes" (the default), "no" or None (meaning
+    #         an answer is required of the user).
+    # The "answer" return value is True for "yes" or False for "no".
+
+    valid = {'yes': True, 'y': True, 'ye': True, 'no': False, 'n': False}
+    if default is None:
+        prompt = '[y/n] '
+    elif default == 'yes':
+        prompt = '[Y/n] '
+    elif default == 'no':
+        prompt = '[y/N] '
+    else:
+        raise ValueError('invalid default answer: [%s]' % default)
+
+    while True:
+        log_print('%s %s' % (question, prompt))
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            log_print('Please respond with "yes" or "no" (or "y" or "n").')
