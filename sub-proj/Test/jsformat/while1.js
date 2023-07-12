ee927dd4306c5ce9a39bf48f85147d069c973d96
Sun Junwen <sunjw8888@gmail.com>
2023-07-12 11:04:05 +0800

    test: Add test cases for "while" format.

---
+++ sub-proj/Test/jsformat/while1.js
@@ -0,0 +1,44 @@
+function p() {
+
+    while(1){
+        b();
+    }
+
+    a=1;
+
+    if(z()){
+        x();
+    }
+
+    while(1){
+        b();
+    }
+
+    p=9;
+    
+    while(1){
+        b();
+    }
+
+    do {
+        c();
+    }
+
+
+
+    while(1);
+}
+
+while(1){
+    j();
+}
+
+do {
+    c();
+}
+
+
+
+while(1);
+
+
