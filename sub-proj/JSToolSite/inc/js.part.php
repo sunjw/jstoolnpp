c6b7b4033d60dc100780adf371b2e8131457ace2
Sun Junwen <sunjw8888@gmail.com>
2025-03-03 13:33:24 +0800

    site: Update to jquery 3.7.1.

--- sub-proj/JSToolSite/inc/js.part.php
+++ sub-proj/JSToolSite/inc/js.part.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="../js/jquery-3.7.1.min.js"></script>
 <script type="text/javascript">
 //<![CDATA[
 var headerLogo = 0;
